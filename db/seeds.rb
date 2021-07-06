d551398c771678731d7795ddf8a664bcd7c9eef4
tokitsu <toki2222son@yahoo.co.jp>
2021-07-07 08:42:49 +0900

    user associate with course

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+User.create!(email: 'admin@email.com', password: 'adminadmin', password_confirmation: 'adminadmin')
+
 30.times do
   Course.create!([{
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quote
+    description: Faker::TvShows::GameOfThrones.quote,
+    user_id: User.first.id
   }])
 end
